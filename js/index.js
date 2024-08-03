b6ef75e9e0e14a39bb8b6dee02951ad086675c55
Shevchenko Andrey <demorest49de@gmail.com>
2024-08-04 00:57:06 +0300

    Changes addItem.css addItemErrorScreen.css basic.css big-red-cross.svg calculations.js caret.svg control.js createElement.js delete.svg edit.svg fileHandler.js filter.svg global.css handleImage.js hero.css hero.js index.html index.js input-search.js Inter-Bold.ttf Inter-Regular.ttf left.svg list-product.css loadModal.js logo.png logo.svg no-img.svg package.json render.js restOperations.js right.svg search.svg style.css toBase64.js trashcan.svg validation.js var.css varsStorage.js yes-img.svg Unversioned Files

--- js/index.js
+++ js/index.js
@@ -1,15 +1,12 @@
 import {renderLanding} from './modules/render.js';
-import {handleControls} from './modules/control.js';
-import {loadGoodsHandler} from './modules/restOperations.js';
-import {getConsts} from './modules/varsStorage.js';
-import {handleSearch} from "./modules/input-search.js";
 
 
 {
-    const init = (selectorApp, title) => {
+    const init = (selectorApp) => {
         const app = document.querySelector(selectorApp);
         
-        const crm = renderLanding(app, title);
+        renderLanding(app);
+        // const crm = renderLanding(app);
         
         // const {URL, verbs, body, imageURL, clearURL, apiCategory} = getConsts();
         // const {addItemBtn, overlay, tbody, form, header, formContent, addItemError, addItemBlock} = crm;
@@ -40,5 +37,25 @@ import {handleSearch} from "./modules/input-search.js";
         // handleSearch(generalVars);
     };
 
-    window.listProductInit = init;
+    window.init = init;
 }
+
+/**
+ * Чтобы удалить последний коммит как локально, так и удаленно, следуйте этим шагам:
+ *
+ * Удаление последнего коммита локально:
+ * Откатите локальный репозиторий к предыдущему коммиту, используя команду git reset:
+ *
+ * bash
+ * Copy code
+ * git reset --hard HEAD~1
+ * Здесь HEAD~1 означает "один коммит назад от текущей позиции HEAD".
+ *
+ * Удаление последнего коммита удаленно:
+ * Теперь вам нужно обновить удаленный репозиторий, чтобы он соответствовал вашему локальному состоянию. Используйте команду git push с флагом --force:
+ *
+ * bash
+ * Copy code
+ * git push origin HEAD --force
+ * Эти команды удалят последний коммит как локально, так и в удаленном репозитории. Будьте осторожны при использовании флага --force, так как он перезапишет историю коммитов на удаленном репозитории и может повлиять на других разработчиков, работающих с этим репозиторием.
+ */
--- js/modules/render.js
+++ js/modules/render.js
@@ -1,38 +1,9 @@
-import {createRow} from './createElement.js';
-import createElement from './createElement.js';
+import {getHero} from "./Hero/hero.js";
 
-const {
-  createSection,
-  createHeader,
-  createMainBlock,
-  createOverlay,
-} = createElement;
-
-export const renderItems = (storage, $) => {
-  while ($.tbody.firstChild) {
-    $.tbody.removeChild($.tbody.firstChild);
-  }
-
-  Object.values(storage).forEach((value) => {
-    const row = createRow(value);
-    $.tbody.append(row);
-  });
-};
-
-export const renderLanding = (app, title) => {
-  const section = createSection();
-  const sectionContainer = section.container;
-  const header = createHeader(title);
-
-  const {mainBlock, tbody, addItemBtn} = createMainBlock();
-
-  sectionContainer.append(header);
-  sectionContainer.append(mainBlock);
-
-  const {overlay, form, formContent, addItemError, addItemBlock} = createOverlay();
-
-
-  app.append(section);
-
-  return {addItemBtn, overlay, tbody, form, header, formContent, addItemError, addItemBlock};
+export const renderLanding = (app) => {
+  
+  const hero = getHero();
+  
+  const arr = [hero]
+  app.append(...arr);
 };
