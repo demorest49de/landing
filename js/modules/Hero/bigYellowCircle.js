bedda1172db3c9a3dcb84f2c5d0584449b00827e
Shevchenko Andrey <demorest49de@gmail.com>
2024-08-06 00:13:25 +0300

    Changes bigYellowCircle.js hero.css

--- js/modules/Hero/bigYellowCircle.js
+++ js/modules/Hero/bigYellowCircle.js
@@ -8,14 +8,6 @@ export const getBigYellowCircle = (parentName) => {
   const svgPath = 'public/img/yellow-circle.svg';
   const circle = createSvg(svgPath, parentName, `circle`);
   
-  circle.addEventListener('mouseover', function () {
-    circle.classList.add('hero__circle_rotate');
-  });
-  
-  circle.addEventListener('click', function ({currentTarget}) {
-    console.log(' currentTarget: ', currentTarget);
-  });
-  
   block.appendChild(circle);
   return block;
 };
