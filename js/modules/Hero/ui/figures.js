613ab721365371fd9a41832966146a41c1acc373
Shevchenko Andrey <demorest49de@gmail.com>
2024-08-06 13:02:58 +0300

    Changes figures.js hero.css

--- js/modules/Hero/ui/figures.js
+++ js/modules/Hero/ui/figures.js
@@ -2,10 +2,10 @@ import {createDiv, createImg} from "../../createComponent.js";
 
 export const getFigures = (parentName) => {
   
-  const peshka = {path: 'public/img/chess-figures/peshka.png', alt: 'фигура пешки', className: 'peshka'};
-  const slon = {path: 'public/img/chess-figures/slon.png', alt: 'фигура слона', className: 'slon'};
-  const korol = {path: 'public/img/chess-figures/korol.png', alt: 'фигура короля', className: 'korol'};
-  const koroleva = {path: 'public/img/chess-figures/koroleva.png', alt: 'фигура королевы', className: 'koroleva'};
+  const peshka = {path: 'public/img/chess-figures/peshka.png', alt: 'фигура пешки', className: 'figure'};
+  const slon = {path: 'public/img/chess-figures/slon.png', alt: 'фигура слона', className: 'figure'};
+  const korol = {path: 'public/img/chess-figures/korol.png', alt: 'фигура короля', className: 'figure'};
+  const koroleva = {path: 'public/img/chess-figures/koroleva.png', alt: 'фигура королевы', className: 'figure'};
   
   const peshkaIMG = createImg(peshka, parentName);
   const slonIMG = createImg(slon, parentName);
