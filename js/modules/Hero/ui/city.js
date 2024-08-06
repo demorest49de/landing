dc97ca8cbab2740a377986a9929858d3d23fb7bd
Shevchenko Andrey <demorest49de@gmail.com>
2024-08-06 15:16:40 +0300

    Changes city.js city.png

--- js/modules/Hero/ui/city.js
+++ js/modules/Hero/ui/city.js
@@ -1,12 +1,14 @@
-import {createSvg} from "../../createComponent.js";
+import {createImg, createSvg} from "../../createComponent.js";
 
 export const getCity = (parentName) => {
+  
+  // const cityObject = {path: 'public/img/city/city.png', alt: 'город на шахматной доске', className: 'city'};
+  //   const city = createImg(cityObject, parentName);
   
   const svgPath = 'public/img/city/city.svg';
   const city = createSvg(svgPath, parentName, `city`);
   
-  
-  const svgPath2 = 'public/img/city/path.svg';
+    const svgPath2 = 'public/img/city/path.svg';
   const path = createSvg(svgPath2, parentName, `path`);
   const elements = [
     city,
