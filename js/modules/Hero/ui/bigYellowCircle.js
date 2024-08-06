7bb1950e9e23555a63db4f9f760da54dff94d466
Shevchenko Andrey <demorest49de@gmail.com>
2024-08-06 22:53:35 +0300

    Changes bigYellowCircle.js circle-words.png hero.css yellow-circle.png

--- js/modules/Hero/ui/bigYellowCircle.js
+++ js/modules/Hero/ui/bigYellowCircle.js
@@ -1,13 +1,17 @@
-import {createDiv, createSvg} from "../../createElement.js";
+import {createDiv, createFigure, createImg, createSvg} from "../../createElement.js";
 
 export const getBigYellowCircle = (parentName) => {
   
   
-  const block = createDiv(parentName, `big-yellow-circle-block`);
+  const circle = createFigure(parentName, `circle`);
   
-  const svgPath = 'public/img/yellow-circle.svg';
-  const circle = createSvg(svgPath, parentName, `circle`);
+  const circleObject = {path: 'public/img/yellow-circle/yellow-circle.png', alt: 'желтый круг', className: 'circle-image'};
+  const circleIMG = createImg(circleObject, parentName);
+  circle.appendChild(circleIMG);
   
-  block.appendChild(circle);
-  return block;
+  // const svgPath = 'public/img/yellow-circle.svg';
+  // const circle = createSvg(svgPath, parentName, `circle`);
+  // block.appendChild(circle);
+  
+  return circle;
 };
